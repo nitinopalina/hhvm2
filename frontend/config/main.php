817c883e2965d55a7ed4d5934ba1cfe5093b2b22
discreet-saurabh <saurabh.opalina@gmail.com>
2018-11-30 15:17:40 +0530

    ch

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
